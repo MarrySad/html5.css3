42ac0dbdabb36d1049043057f22052c078487891
MarrySad <mvrsad@gmail.com>
2018-11-23 18:00:47 +0700

    альтернативный хедер

---
+++ project2/form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Base</title>
+</head>
+<body>
+
+<form action="">
+    <fieldset>
+        <input type="text">
+    </fieldset>
+</form>
+
+</body>
+</html>
